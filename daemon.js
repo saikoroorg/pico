b38529fa5b962be3040cff3301c0cc763e1b06cb
@ <@>
2023-09-30 00:31:30 +0900

    Add image/sound module.

---
+++ daemon.js
@@ -0,0 +1,450 @@
+/* PICO Daemon module */
+
+//************************************************************/
+
+// Namespace.
+var pico = pico || {};
+
+// Worker class.
+pico.Worker = class {
+	static script = "./daemon.js"; // This script file.
+	static manifest = "./manifest.json"; // Manifest file.
+
+	//*----------------------------------------------------------*/
+
+	// constructor.
+	constructor() {
+		this.replacing = {}; // Replacing table by manifest json.
+		this.cacheKey = null; // Cache key.
+		this.cacheName = null; // Cache key without version.
+	}
+
+	// Get file from cache or fetch.
+	_cacheOrFetch(url, cacheKey, replacing) {
+		return new Promise((resolve, reject) => {
+			if (cacheKey) {
+
+				// Get cached file.
+				console.log("Get cached file: " + url + " from " + cacheKey);
+				self.caches.open(cacheKey).then((cache) => {
+					cache.match(url, {ignoreSearch: true}).then((result) => {
+
+						// Resolves by cached response.
+						console.log("Resolves by cached response: " + url + " -> " + result.status + " " + result.statusText);
+						resolve(result);
+					}).catch((error) => {
+
+						// Not found cached file.
+						console.log("Not found cached file: " + url);
+						this._fetchAndCache(url, cacheKey, replacing).then((result) => {
+
+							// Resolves by fetched response.
+							console.log("Resolves by fetched response: " + url + " -> " + result.status + " " + result.statusText);
+							resolve(result);
+						});
+					});
+				});
+
+			} else {
+
+				// No cache.
+				console.log("No cache: " + url);
+				fetch(url).then((result) => {
+
+					// Resolves by fetched response.
+					console.log("Resolves by fetched response: " + url + " -> " + result.status + " " + result.statusText);
+					resolve(result);
+				});
+			}
+		}); // end of new Promise.
+	}
+
+	// Fetch and cache.
+	_fetchAndCache(url, cacheKey, replacing) {
+		return fetch(url, {cache: "no-store"}).then((result) => {
+
+			// Cache the fetched file.
+			console.log("Fetched file: " + url + " -> " + result.status + " " + result.statusText);
+			let contentType = result.headers.get("Content-Type");
+			if (!contentType.match("text/html")) {
+				if (cacheKey) {
+					console.log("Cache the fetched file: " + url + " to " + cacheKey + " -> " + result.status + " " + result.statusText);
+					self.caches.open(cacheKey).then((cache) => {
+						cache.put(url, result.clone());
+					});
+				}
+
+				// Returns fetched response.
+				console.log("Returns fetched response. -> " + result.status + " " + result.statusText);
+				return result.clone();
+
+			// Replace fetched html file.
+			} else {
+				return new Promise((resolve, reject) => {
+					result.text().then((text) => {
+						console.log("Fetched html file: " + text.replace(/\s+/g, " ").substr(-1000));
+
+						// Replace strings by manifest.
+						for (let key in replacing) {
+							console.log("Replacing: " + key + " -> " + replacing[key]);
+							let reg1 = new RegExp("(<.*id=\"" + key + "\".*>).*(<\/.*>)");
+							text = text.replace(reg1, "$1" + replacing[key] + "$2");
+							let reg2 = new RegExp("(<" + key + ".*>).*(<\/" + key + ".*>)");
+							text = text.replace(reg2, "$1" + replacing[key] + "$2");
+						}
+						console.log("Replaced file: " + text.replace(/\s+/g, " ").substr(-1000));
+
+						// Replaced responce.
+						let options = {status: result.status,
+							 statusText: result.statusText,
+							 headers: result.headers};
+						result = new Response(text, options);
+
+						// Cache the replaced file.
+						if (cacheKey) {
+							console.log("Cache the replaced file: " + url + " to " + cacheKey + " -> " + result.status + " " + result.statusText);
+							self.caches.open(cacheKey).then((cache) => {
+								cache.put(url, result.clone());
+							});
+						}
+
+						// Resolves by replaced response.
+						console.log("Resolves by replaced response: " + url + " -> " + result.status + " " + result.statusText);
+						resolve(result.clone());
+					});
+				}); // end of new Promise.
+			}
+		});
+	}
+
+	// Prefetch all content files to renew.
+	_renew() {
+		return new Promise((resolve, reject) => {
+			console.log("Renew worker.");
+
+			// Fetch new manifest file.
+			let url = pico.Worker.manifest;
+			console.log("Fetch new manifest: " + url);
+			return fetch(url, {cache: "no-store"}).then((result) => {
+
+				// Check countent type.
+				let contentType = result.headers.get("Content-Type");
+				if (!contentType.match("application/json")) {
+					console.log("Failed to parse manifest file.");
+					reject();
+					return;
+				}
+
+				// Parse manifest json.
+				result.clone().json().then((manifest) => {
+					console.log("Parsed new manifest json: " + JSON.stringify(manifest));
+					let cacheKey = manifest.name + "/" + manifest.version;
+					let replacing = this._replacing(manifest);
+
+					// Not found new version.
+					if (cacheKey == this.cacheKey) {
+						console.log("Not found new version: " + cacheKey);
+						resolve(result.clone());
+
+					// Found new version.
+					} else {
+						console.log("Found new version: " + cacheKey + " old: " + this.cacheKey);
+
+						// Prefetch and cache all content files.
+						console.log("Prefetch all content files.");
+						Promise.all(manifest.contents.map((content) => {
+							return this._fetchAndCache(content, cacheKey, replacing);
+						})).then(() => { // end of Promise.all.
+
+							// Cache new manifest.
+							cacheKey = "*";
+							console.log("Cache new manifest: " + url + " to " + cacheKey + " -> " + result.status + " " + result.statusText);
+							self.caches.open(cacheKey).then((cache) => {
+								cache.put(url, result.clone());
+
+								// Resolves.
+								console.log("Renew worker completed.");
+								resolve(result.clone());
+							});
+						});
+					}
+				});
+			}).catch((error) => {
+				console.log("Failed to parse manifest file.");
+				console.error(error.name, error.message);
+				reject();
+			});
+		}); // end of new Promise.
+	}
+
+	// Check new manifest file.
+	_check() {
+		return new Promise((resolve, reject) => {
+			console.log("Check new maniefst file.");
+
+			// Fetch new manifest file.
+			let url = pico.Worker.manifest;
+			console.log("Fetch new manifest: " + url);
+			return fetch(url, {cache: "no-store"}).then((result) => {
+
+				// Check countent type.
+				let contentType = result.headers.get("Content-Type");
+				if (!contentType.match("application/json")) {
+					console.log("Failed to parse manifest file.");
+					reject();
+					return;
+				}
+
+				// Parse manifest json.
+				result.clone().json().then((manifest) => {
+					console.log("Parsed new manifest json: " + JSON.stringify(manifest));
+					let cacheKey = manifest.name + "/" + manifest.version;
+					let replacing = this._replacing(manifest);
+
+					// Not found new version.
+					if (cacheKey == this.cacheKey) {
+						console.log("Not found new version: " + cacheKey);
+						resolve(result.clone());
+
+					// Found new version.
+					} else {
+						console.log("Found new version: " + cacheKey + " old: " + this.cacheKey);
+						reject();
+					}
+				});
+			}).catch((error) => {
+				console.log("Failed to fetch manifest file.");
+				console.error(error.name, error.message);
+				reject();
+			});
+		}); // end of new Promise.
+	}
+
+	// Replacing table.
+	_replacing(manifest) {
+		let replacing = {};
+		if (manifest.version) {
+			if (manifest.name) {
+				replacing.version = manifest.name + "#" + manifest.version.substr(-4);
+			} else {
+				replacing.version = "#" + manifest.version.substr(-4);
+			}
+		}
+		if (manifest.author) {
+			replacing.author = manifest.author;
+		}
+		if (manifest.short_name) {
+			replacing.title = manifest.short_name;
+		}
+		console.log("Created replacing table: " + JSON.stringify(replacing));
+		return replacing;
+	}
+
+	// Set manifest file to start worker.
+	_start(result) {
+		if (this.cacheKey) {
+			console.log("Worker already started.");
+			return Promise.resolve();
+		}
+		return new Promise((resolve, reject) => {
+			console.log("Start worker.");
+
+			// Get cached manifest file.
+			let url = pico.Worker.manifest, cacheKey = "*";
+			console.log("Get cached manifest file: " + url + " from " + cacheKey);
+			self.caches.open(cacheKey).then((cache) => {
+				cache.match(url, {ignoreSearch: true}).then((result) => {
+					console.log("Found manifest file: " + url + " from " + cacheKey);
+
+					// Check result.
+					if (!result) {
+						console.log("Failed to get manifest file.");
+						reject();
+						return;
+					}
+
+					// Check countent type.
+					let contentType = result.headers.get("Content-Type");
+					if (!contentType.match("application/json")) {
+						console.log("Failed to parse manifest file.");
+						reject();
+						return;
+					}
+
+					// Parse manifest json.
+					result.clone().json().then((manifest) => {
+						console.log("Parsed manifest json: " + JSON.stringify(manifest));
+
+						// Set version and cache key.
+						this.cacheKey = manifest.name + "/" + manifest.version;
+						this.cacheName = manifest.name;
+						console.log("Set version: " + this.cacheKey);
+
+						// Set replacing table.
+						this.replacing = this._replacing(manifest);
+
+						// Resolves.
+						console.log("Start worker completed.");
+						resolve(result.clone());
+					});
+				}).catch((error) => {
+					console.log("Failed to parse manifest file.");
+					console.error(error.name, error.message);
+					reject();
+				});
+			});
+		}); // end of new Promise.
+	}
+	
+	// Get manifest file from cache or fetch on installing worker.
+	install() {
+		return new Promise((resolve) => {
+
+			// Check manifest file to use cache.
+			this._check().then((result) => {
+				console.log("Worker already installed.");
+				resolve(result);
+			}).catch((error) => {
+				console.log("Reinstall worker.");
+
+				// Refetch manifest file.
+				let url = pico.Worker.manifest, cacheKey = "*", replacing = null;
+				console.log("Refetch manifest file: " + url);
+				this._fetchAndCache(url, cacheKey, replacing).then((result) => {
+
+					// Resolves.
+					console.log("Install worker completed.");
+					resolve(result);
+
+					// Prefetch all content files on background for this install.
+					this._renew();
+				}).catch((error) => {
+
+					// Failed but resolves to continue worker.
+					console.log("Failed to install worker.");
+					resolve();
+				});
+			});
+		}); // end of new Promise.
+	}
+
+	// Delete old cache files when the cache version updated on activating worker.
+	activate() {
+		return new Promise((resolve) => {
+
+			// Read manifest file to use cache.
+			this._start().then(() => {
+
+				// Check cache name.
+				if (!this.cacheName) {
+					console.log("Failed to get manifest file.");
+					reject();
+					return;
+				}
+
+				// Delete all cache files.
+				console.log("Delete all cache files: " + this.cacheName);
+				self.caches.keys().then((keys) => {
+					Promise.all(keys.map((key) => {
+						if (!key.indexOf(this.cacheName) && key != this.cacheKey) {
+							console.log("Delete old cache: " + key);
+							return self.caches.delete(key);
+						}
+					})).then(() => { // end of Promise.all.
+
+						// Resolves.
+						console.log("Delete all cache files completed.");
+						resolve();
+					});
+				});
+			}).catch((error) => {
+
+				// Failed but resolves to continue worker.
+				console.log("Failed to activate worker.");
+				resolve();
+			});
+		}); // end of new Promise.
+	}
+
+	// Get cache or fetch files called by app.
+	fetch(url) {
+		return new Promise((resolve) => {
+
+			// Read manifest file to use cache.
+			this._start().then(() => {
+
+				// Get cache or fetch and return response.
+				console.log("Get cache or fetch");
+				this._cacheOrFetch(url, this.cacheKey, this.replacing).then((result) => {
+
+					// Resolves.
+					console.log("Fetch by worker completed: " + url + " -> " + result.status + " " + result.statusText);
+					resolve(result);
+
+					// Prefetch all content files on background for next install.
+					this._renew();
+				}).catch((error) => {
+
+					// Failed but resolves to continue worker.
+					console.log("Failed to fetch by worker.");
+					resolve(fetch(url)); // Simple fetch.
+				});
+			});
+		}); // end of new Promise.
+	}
+};
+
+// Master worker.
+pico.worker = new pico.Worker();
+
+// Script for client to register worker.
+if (!self || !self.registration) {
+	try {
+
+		// Register worker.
+		if (pico.Worker.script) {
+			if (navigator.serviceWorker) {
+				console.log("Register worker: " + pico.Worker.script);
+				(async()=>{
+					await navigator.serviceWorker.register(pico.Worker.script);
+				})();
+			} else {
+				console.log("No worker.");
+			}
+		}
+
+		// Wake lock.
+		// Not work on iOS 16 PWA.
+		// https://bugs.webkit.org/show_bug.cgi?id=254545
+		if (navigator.wakeLock) {
+			console.log("Request wake lock.");
+			navigator.wakeLock.request("screen");
+		} else {
+			console.log("No wake lock.");
+		}
+
+	} catch (error) {
+		console.error(error.name, error.message);
+	}
+
+// Script for worker.
+} else {
+
+	// Event on installing worker.
+	self.addEventListener("install", (event) => {
+		console.log("Install worker: " + pico.Worker.script);
+		event.waitUntil(pico.worker.install());
+	});
+
+	// Event on activating worker.
+	self.addEventListener("activate", (event) => {
+		console.log("Activate worker: " + pico.Worker.script);
+		event.waitUntil(pico.worker.activate());
+	});
+
+	// Event on fetching network request.
+	self.addEventListener("fetch", (event) => {
+		console.log("Fetch by worker: " + event.request.url);
+		event.respondWith(pico.worker.fetch(event.request.url));
+	});
+}
---
+++ image.js
@@ -0,0 +1,279 @@
+/* PICO Image module */
+
+// Random.
+function picoRandom(max) {
+	return pico.image.random(max);
+}
+
+// Wait and flip canvas.
+async function picoFlip(t=10) {
+	try {
+		await pico.image.flip(t);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+// Clear canvas.
+async function picoClear() {
+	try {
+		await pico.image.clear();
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+// Set color pallete.
+async function picoColor(colors=[0,0,0]) {
+	try {
+		pico.image.color(colors);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+// Draw rects.
+async function picoDraw(cells=[1,0,0], x=0, y=0, angle=0, scale=1) {
+	try {
+		await pico.image.draw(cells, x, y, angle, scale);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+//************************************************************/
+
+// Namespace.
+var pico = pico || {};
+
+// Image class.
+pico.Image = class {
+	static width = 200; // Canvas width.
+	static height = 200; // Canvas height.
+	static unit = 4; // Unit size. (Requires multiple of 2 for center pixel)
+	static lock = "picoImageLock"; // Lock object identifier.
+	static parent = "screen"; // Parent pane id.
+
+	// Get random count.
+	random(max) {
+
+		// Xorshift algorythm.
+		this.seed = this.seed ^ (this.seed << 13);
+		this.seed = this.seed ^ (this.seed >>> 17);
+		this.seed = this.seed ^ (this.seed << 15);
+		return Math.abs(this.seed % max);
+
+		// LCG algorythm.
+		// this.seed = (this.seed * 9301 + 49297) % 233280;
+		// let rand = this.seed / 233280;
+		// return Math.round(rand * max);
+	}
+
+	// Wait and flip canvas.
+	flip(t=10) {
+		return new Promise(r => setTimeout(r, t)).then(() => {
+			return navigator.locks.request(pico.Image.lock, async (lock) => {
+				return this._flip();
+			});
+		});
+	}
+
+	// Clear canvas.
+	clear() {
+		return navigator.locks.request(pico.Image.lock, async (lock) => {
+			return this._clear();
+		});
+	}
+
+	// Set color pallete.
+	color(colors=[0,0,0]) {
+		return navigator.locks.request(pico.Image.lock, async (lock) => {
+			this.colors = colors;
+		});
+	}
+
+	// Draw rects to canvas.
+	draw(cells=[1,0,0], x=0, y=0, angle=0, scale=1) {
+		return navigator.locks.request(pico.Image.lock, async (lock) => {
+			return this._ready().then(() => {
+				return this._reset();
+			}).then(() => {
+				return this._move(x, y);
+			}).then(() => {
+				return this._rotate(angle);
+			}).then(() => {
+				return this._scale(scale);
+			}).then(() => {
+				return this._drawRect(cells);
+			});
+		});
+	}
+
+	//*----------------------------------------------------------*/
+
+	// constructor.
+	constructor(parent=null) {
+		this.canvas = []; // Canvas element.
+		this.primary = 0; // Primary index.
+		this.image = null; // Canvas 2d context.
+		this.colors = [0,0,0, 51,51,51, 255,255,255]; // Color pallete. 
+		this.seed = Date.now(); // Random seed.
+
+		// Setup now.
+		if (parent) {
+			this._setup(parent);
+
+		// Setup after load event.
+		} else {
+			window.addEventListener("load", () => {
+				this._setup(pico.Image.parent);
+			});
+		}
+	}
+
+	// Setup image.
+	_setup(parent=null) {
+		return new Promise((resolve) => {
+
+			// Create image.
+			if (this.image == null) {
+				console.log("Create image.");
+				for (let i = 0; i < 2; i++) {
+					this.canvas[i] = document.createElement("canvas");
+					this.canvas[i].width = pico.Image.width;
+					this.canvas[i].height = pico.Image.height;
+					this.canvas[i].style.imageRendering = "pixelated";
+					this.canvas[i].style.display = i == this.primary ? "flex" : "none";
+					if (parent && document.getElementById(parent)) {
+						document.getElementById(parent).appendChild(this.canvas[i]);
+					} else {
+						document.body.appendChild(this.canvas[i]);
+					}
+				}
+				this.image = this.canvas[this.primary].getContext("2d");
+			}
+			return Promise.resolve();
+		}); // end of new Promise.
+	}
+
+	// Flip canvas.
+	_flip() {
+		return this._ready().then(() => {
+			console.log("Flip.");
+			return new Promise((resolve) => {
+				for (let i = 0; i < 2; i++) {
+					this.canvas[i].style.display = i == this.primary ? "flex" : "none";
+				}
+				this.primary = this.primary != 0 ? 0 : 1;
+				this.image = this.canvas[this.primary].getContext("2d");
+				resolve();
+			}); // end of new Promise.
+		});
+	}
+
+	// Clear canvas.
+	_clear() {
+		return this._ready().then(() => {
+			console.log("Clear.");
+			return new Promise((resolve) => {
+
+				// Clear canvas.
+				this.image.setTransform(1, 0, 0, 1, 0, 0);
+				this.image.clearRect(0, 0, pico.Image.width, pico.Image.height);
+
+				// Clip by canvas rect.
+				this.image.rect(0, 0, pico.Image.width, pico.Image.height);
+				this.image.clip();
+
+				resolve();
+			}); // end of new Promise.
+		});
+	}
+
+	// Ready to start.
+	_ready() {
+		if (this.image == null) {
+			console.log("No image.");
+			return Promise.reject();
+		}
+		return Promise.resolve();
+	}
+
+	// Reset image transform (scale, rotate, move).
+	_reset() {
+		console.log("Reset transform matrix.");
+		return new Promise((resolve) => {
+			this.image.setTransform(1, 0, 0, 1, 0, 0);
+			resolve();
+		}); // end of new Promise.
+	}
+
+	// Scale image.
+	_scale(scale=1) {
+		console.log("Scale: " + scale);
+		return new Promise((resolve) => {
+			if (scale != 1) {
+				this.image.scale(scale, scale);
+			}
+			resolve();
+		}); // end of new Promise.
+	}
+
+	// Rotate image.
+	_rotate(angle=0) {
+		console.log("Rotate: " + angle);
+		return new Promise((resolve) => {
+			if (angle) {
+				this.image.translate(pico.Image.width / 2, pico.Image.height / 2);
+				this.image.rotate(angle * Math.PI / 180);
+				this.image.translate(-pico.Image.width / 2, -pico.Image.height / 2);
+			}
+			resolve();
+		}); // end of new Promise.
+	}
+
+	// Move image.
+	_move(x, y) {
+		console.log("Move: " + x + "," + y);
+		return new Promise((resolve) => {
+			if (x || y) {
+				this.image.translate(x, y);
+			}
+			resolve();
+		}); // end of new Promise.
+	}
+
+	// Draw rects.
+	_drawRect(cells=[1,0,0]) {
+		console.log("Draw: " + cells + " x " + cells.length);
+		return new Promise((resolve) => {
+
+			const ux = pico.Image.unit, uy = pico.Image.unit;
+			const cx = pico.Image.width / 2 - ux / 2, cy = pico.Image.height / 2 - uy / 2;
+			//console.log("Center: " + cx + "," + cy + " x " + ux + "," + uy);
+
+			for (let i = 0; i < cells.length; i += 3) {
+				let k = cells[i] >= 0 && cells[i] * 3 < this.colors.length ? cells[i] * 3 : 0;
+				let r = this.colors[k], g = this.colors[k+1], b = this.colors[k+2];
+				//console.log("Color: " + r + "," + g + "," + b);
+
+				let x = cells[i+1] ? cells[i+1] * ux + cx : cx;
+				let y = cells[i+2] ? cells[i+2] * uy + cy : cy;
+				let w = ux, h = uy;
+				if (cells[i+3] == 0) {
+					w = cells[i+4] ? cells[i+4] * ux : ux;
+					h = cells[i+5] ? cells[i+5] * uy : uy;
+					i += 3;
+					//console.log("Rect: " + x + "," + y + " x " + w + "," + h);
+				}
+
+				this.image.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+				this.image.fillRect(x, y, w, h);
+			}
+			resolve();
+		}); // end of new Promise.
+	}
+};
+
+// Master image.
+pico.image = new pico.Image();
