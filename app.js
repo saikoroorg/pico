e1ecad762491cb49bfcff8e11a228c280dc22232
@ <@>
2024-01-11 18:59:24 +0900

    Split app main method.

--- app.js
+++ app.js
@@ -62,6 +62,11 @@ var level = 1; // Playing level.
 var maxlevel = 1; // Cleared level.
 var playing = 1; // Playing count.
 var blocking = -1; // Blocking count.
+const maxsize = 20; // Max field size.
+var width = 7, height = 7; // Field size.
+var grid = 24, margin = 2, primary = 0;
+var pixels = [[],[],[],[],[],[],[]];
+var players = [[], []];
 
 // Action button.
 async function appAction() {
@@ -122,8 +127,8 @@ async function appSelect(x) {
 	return 0; // Do nothing.
 }
 
-// Main.
-async function appMain() {
+// Load.
+async function appLoad() {
 
 	// Load query params.
 	let value = picoStrings();
@@ -151,161 +156,156 @@ async function appMain() {
 
 	// Load pallete data.
 	picoColor(colors);
+}
 
-	// Main loop.
-	for (;;) {
+// Init.
+async function appInit() {
 
-		// Initial param.
-		const maxsize = 24; // Max size.
-		let width = 7, height = 7; // Field size.
-		let grid = 24, margin = 2, primary = 0;
-		let pixels = [[],[],[],[],[],[],[]];
-		let players = [[], []];
+	// Init level data.
+	if (levels[level]) {
+		for (let k = 0; k < levels[level].length / 3; k++) {
+			let w = levels[level][k * 3];
+			let x = levels[level][k * 3 + 1];
+			let y = levels[level][k * 3 + 2];
+			if (w == 0) {
+				width = x >= 0 && x <= maxsize ? x : 7;
+				height = y >= 0 && y <= maxsize ? y : 7;
+				grid = 168 / width;
+				margin = width <= 9 ? 2 : 1;
+				players = [[0, 0], [width - 1, height - 1]];
+				for (let j = 0; j < height; j++) {
+					pixels[j] = [];
+					for (let i = 0; i < width; i++) {
+						pixels[j][i] = 0;
+					}
+				}
+				blocking = width * height;
+			} else if (w >= 1 && w <= 4 && x >= 0 && x < width && y >= 0 && y < height) {
+				if (w >= 1 && w <= 2) {
+					if (pixels[players[w - 1][1]][players[w - 1][0]] >= 3) {
+						pixels[players[w - 1][1]][players[w - 1][0]] -= w;
+					} else {
+						pixels[players[w - 1][1]][players[w - 1][0]] = 0;
+					}
+					players[w - 1] = [x, y];
+				} else if (w == 3) {
+					pixels[players[0][1]][players[0][0]] = 0;
+					pixels[players[1][1]][players[1][0]] = 0;
+					players[0] = [x, y];
+					players[1] = [x, y];
+				}
+				pixels[y][x] = w;
+				blocking -= 1;
+			}
+		}
+	}
 
-		// Load level data.
-		if (levels[level]) {
-			for (let k = 0; k < levels[level].length / 3; k++) {
-				let w = levels[level][k * 3];
-				let x = levels[level][k * 3 + 1];
-				let y = levels[level][k * 3 + 2];
-				if (w == 0) {
-					width = x >= 0 && x <= maxsize ? x : 7;
-					height = y >= 0 && y <= maxsize ? y : 7;
-					grid = 168 / width;
-					margin = width <= 9 ? 2 : 1;
-					players = [[0, 0], [width - 1, height - 1]];
-					for (let j = 0; j < height; j++) {
-						pixels[j] = [];
-						for (let i = 0; i < width; i++) {
-							pixels[j][i] = 0;
+	// Reset playing count.
+	playing = 1;
+}
+
+// Main.
+async function appMain() {
+
+	// Move player.
+	for (let j = 0; j < height; j++) {
+		let y = (j - (height - 1) / 2) * grid;
+		for (let i = 0; i < width; i++) {
+			let x = (i - (width - 1) / 2) * grid;
+
+			// Touch blank cell to move player.
+			if (pixels[j][i] == 0) {
+				if (picoMotion(x, y, grid/2)) {
+					// Check cell next to player.
+					let next = [false, false];
+					for (let k = 0; k < 2; k++) {
+						let dx = i - players[k][0], dy = j - players[k][1];
+						if (dx * dy == 0 && (dx + dy == -1 || dx + dy == 1)) {
+							next[k] = true;
 						}
 					}
-					blocking = width * height;
-				} else if (w >= 1 && w <= 4 && x >= 0 && x < width && y >= 0 && y < height) {
-					if (w >= 1 && w <= 2) {
-						if (pixels[players[w - 1][1]][players[w - 1][0]] >= 3) {
-							pixels[players[w - 1][1]][players[w - 1][0]] -= w;
-						} else {
-							pixels[players[w - 1][1]][players[w - 1][0]] = 0;
+					// Check facing cell between 2 players.
+					let dx = players[0][0] - players[1][0], dy = players[0][1] - players[1][1];
+					let next2 = dx * dy == 0 && (dx + dy == 2 || dx + dy == -2) ? true : false;
+					// P1 and P2 move into same cell.
+					if (next[0] && next[1] && next2) {
+						pixels[j][i] = 3;
+						players[0] = players[1] = [i, j];
+						blocking -= 1;
+					} else {
+						// Move primary player first.
+						for (let k = 0; k < 2; k++) {
+							let k1 = primary == 0 ? k : k ? 0 : 1, k2 = k1 ? 0 : 1;
+							// Player A moves forward and another player B moves backward.
+							if (next[k1]) {
+								pixels[j][i] = k1 == 0 ? 1 : 2;
+								let i2 = players[k2][0] + players[k1][0] - i;
+								let j2 = players[k2][1] + players[k1][1] - j;
+								if (i2 >= 0 && i2 < width && j2 >= 0 && j2 < height) {
+									if (pixels[j2][i2] == 0) {
+										pixels[j2][i2] = k2 == 0 ? 1 : 2;
+										players[k2] = [i2, j2];
+										blocking -= 1;
+									}
+								}
+								players[k1] = [i, j];
+								blocking -= 1;
+								break; // Only one player moves forward.
+							}
 						}
-						players[w - 1] = [x, y];
-					} else if (w == 3) {
-						pixels[players[0][1]][players[0][0]] = 0;
-						pixels[players[1][1]][players[1][0]] = 0;
-						players[0] = [x, y];
-						players[1] = [x, y];
 					}
-					pixels[y][x] = w;
-					blocking -= 1;
+				}
+
+				// Clear level.
+				if (blocking == 0) {
+					players = [[], []];
+					maxlevel = level + 1;
+					picoBeep(1.2, 0.1);
+					picoBeep(1.2, 0.1, 0.2);
+
+					// Update action button to share.
+					picoLabel("action", "^");
 				}
 			}
 		}
+	}
 
-		// Playing loop.
-		for (playing = 1; playing > 0; playing++) {
-			picoClear();
+	// Draw player.
+	for (let j = 0; j < height; j++) {
+		let y = (j - (height - 1) / 2) * grid;
+		for (let i = 0; i < width; i++) {
+			let x = (i - (width - 1) / 2) * grid;
+			let expand = false;
 
-			// Move player.
-			for (let j = 0; j < height; j++) {
-				let y = (j - (height - 1) / 2) * grid;
-				for (let i = 0; i < width; i++) {
-					let x = (i - (width - 1) / 2) * grid;
+			// Touch color cell to update primary player.
+			if (pixels[j][i] != 0) {
+				if (picoMotion(x, y, (grid+margin)/2)) {
+					if (pixels[j][i] == 1) {
+						primary = 0;
+					} else if (pixels[j][i] == 2) {
+						primary = 1;
+					}
 
-					// Touch blank cell to move player.
-					if (pixels[j][i] == 0) {
-						if (picoMotion(x, y, grid/2)) {
-							// Check cell next to player.
-							let next = [false, false];
-							for (let k = 0; k < 2; k++) {
-								let dx = i - players[k][0], dy = j - players[k][1];
-								if (dx * dy == 0 && (dx + dy == -1 || dx + dy == 1)) {
-									next[k] = true;
-								}
-							}
-							// Check facing cell between 2 players.
-							let dx = players[0][0] - players[1][0], dy = players[0][1] - players[1][1];
-							let next2 = dx * dy == 0 && (dx + dy == 2 || dx + dy == -2) ? true : false;
-							// P1 and P2 move into same cell.
-							if (next[0] && next[1] && next2) {
-								pixels[j][i] = 3;
-								players[0] = players[1] = [i, j];
-								blocking -= 1;
-							} else {
-								// Move primary player first.
-								for (let k = 0; k < 2; k++) {
-									let k1 = primary == 0 ? k : k ? 0 : 1, k2 = k1 ? 0 : 1;
-									// Player A moves forward and another player B moves backward.
-									if (next[k1]) {
-										pixels[j][i] = k1 == 0 ? 1 : 2;
-										let i2 = players[k2][0] + players[k1][0] - i;
-										let j2 = players[k2][1] + players[k1][1] - j;
-										if (i2 >= 0 && i2 < width && j2 >= 0 && j2 < height) {
-											if (pixels[j2][i2] == 0) {
-												pixels[j2][i2] = k2 == 0 ? 1 : 2;
-												players[k2] = [i2, j2];
-												blocking -= 1;
-											}
-										}
-										players[k1] = [i, j];
-										blocking -= 1;
-										break; // Only one player moves forward.
-									}
-								}
-							}
-						}
-
-						// Clear level.
-						if (blocking == 0) {
-							players = [[], []];
-							maxlevel = level + 1;
-							picoBeep(1.2, 0.1);
-							picoBeep(1.2, 0.1, 0.2);
-
-							// Update action button to share.
-							picoLabel("action", "^");
+				// Set scale for touching or not touching player.
+				} else {
+					for (let k = 0; k < 2; k++) {
+						if (i == players[k][0] && j == players[k][1]) {
+							expand = true;
+							break;
 						}
 					}
 				}
 			}
 
-			// Draw player.
-			for (let j = 0; j < height; j++) {
-				let y = (j - (height - 1) / 2) * grid;
-				for (let i = 0; i < width; i++) {
-					let x = (i - (width - 1) / 2) * grid;
-					let expand = false;
-	
-					// Touch color cell to update primary player.
-					if (pixels[j][i] != 0) {
-						if (picoMotion(x, y, (grid+margin)/2)) {
-							if (pixels[j][i] == 1) {
-								primary = 0;
-							} else if (pixels[j][i] == 2) {
-								primary = 1;
-							}
-
-						// Set scale for touching or not touching player.
-						} else {
-							for (let k = 0; k < 2; k++) {
-								if (i == players[k][0] && j == players[k][1]) {
-									expand = true;
-									break;
-								}
-							}
-						}
-					}
-
-					// Draw cell.
-					if (expand) {
-						picoPixel(pixels[j][i], x, y, (grid+margin)/2, (grid+margin)/2);
-					} else {
-						picoPixel(pixels[j][i], x, y, (grid-margin)/2-1, (grid-margin)/2-1);
-					}
-				}
+			// Draw cell.
+			if (expand) {
+				picoPixel(pixels[j][i], x, y, (grid+margin)/2, (grid+margin)/2);
+			} else {
+				picoPixel(pixels[j][i], x, y, (grid-margin)/2-1, (grid-margin)/2-1);
 			}
+		}
+	}
 
-			await picoFlip();
-			await picoRead();
-		} // End of playing loop.
-	} // End of main loop.
+	// Increment playing count.
+	return playing++;
 }
