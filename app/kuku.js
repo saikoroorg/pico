1adde4984ab4b5589a492ff2b14a8e9d17394390
@ <@>
2024-01-30 07:50:39 +0900

    Unlock extra level when clear maximum level. Fix parameters for each level.

--- app/kuku.js
+++ app/kuku.js
@@ -8,19 +8,28 @@ const kcents = [-1.0,
 	 1.5, 1.7, 1.9,  2.0, 2.2, 2.4];
 
 // Global variables.
+var state = ""; // Playing state.
 var playing = 0; // Playing count.
-var number = 1;
-const numberMax = 20;
-var levels = ["+10", "-10", "-20", "x5", "x9", "x12", "x16", "x19"];
-var samples = ["1 + 1", "2 - 2", "10+10", "5 * 5", "9 * 9", "12*12", "16* 9", "19*19"];
-var level = 4;
-var state = "";
+var number = 1; // Probrem number.
+const maxnumber = 20; // Maximum number for one play.
+const levels = [ // Parameters for each level.
+	["",0], // Custom level.
+	["+",5], ["+",10], ["+",20], // Add/Sub levels.
+	["x",5,5], ["x",9,9], ["x",12,12], ["x",16,9], ["x",19,19], // Mul levels.
+	["x",90,9], ["x",99,99]]; // Extra levels.
+var level = 5; // Level index.
+var extra = 0; // Enable extra levels.
+const maxextra = 2; // Maximum extra levels.
 var seed = 0; // Random seed.
 
-// Probrem generate parameters.
-var param0 = "x";
-var param1 = 9;
-var param2 = 9;
+// Probrem text.
+function appProbremText(p0, p1, p2) {
+	if (p2 != null) {
+		return (p1 < 10 ? p1 + " " : p1) + (p0 == "x" ? "*" : p0) + (p2 < 10 ? " " + p2 : p2);
+	} else {
+		return p0 + p1;
+	}
+}
 
 // Probrem and answer.
 var probrem1 = 0;
@@ -37,37 +46,18 @@ var totalTime = 0;
 
 // Select button.
 async function appSelect(x) {
-	if (level < 0) {
+	if (level <= 0) {
 		picoBeep(-1.2, 0.1);
 	} else if (x) {
-		level = picoMod(level + x + levels.length, levels.length);
-		picoLabel("select", levels[level]);
+		let maxlevel = levels.length-1 - maxextra + extra;
+		level = picoMod(level-1 + x + maxlevel, maxlevel) + 1;
+		picoLabel("select", appProbremText(levels[level][0], levels[level][1]));
 		picoBeep(1.2, 0.1);
-	}
 
-	// Load level.
-	if (level == 0) {
-		param0 = "+", param1 = param2 = 10; // Add up to 10.
-	} else if (level == 1) {
-		param0 = "-", param1 = param2 = 10; // Add/Sub up to 10.
-	} else if (level == 2) {
-		param0 = "-", param1 = param2 = 20; // Add/Sub up to 20.
-	} else if (level == 3) {
-		param0 = "*", param1 = param2 = 5; // Mul 1x1 to 5x5.
-	} else if (level == 4) {
-		param0 = "*", param1 = param2 = 5; // Mul 1x1 to 5x5.
-	} else if (level == 4) {
-		param0 = "*", param1 = param2 = 9; // Mul 1x1 to 9x9.
-	} else if (level == 5) {
-		param0 = "*", param1 = param2 = 12; // Mul 2x2 to 12x12.
-	} else if (level == 6) {
-		param0 = "*", param1 = 16, param2 = 9; // Mul 11x2 to 16x9.
-	} else if (level == 7) {
-		param0 = "*", param1 = 19, param2 = 19; // Mul 11x2 to 19x19.
-	} else if (level == 8) {
-		param0 = "*", param1 = 31, param2 = 31; // Mul 11x2 to 31x31.
-	} else {
-		param0 = "*", param1 = 99, param2 = 99; // Mul 11x2 to 99x99.
+		// Unlock extra levels.
+		/*if (x > 0 && level == 1) {
+			extra = extra + 1 < maxextra ? extra + 1 : maxextra;
+		}*/
 	}
 
 	// Restart.
@@ -86,33 +76,30 @@ async function appAction() {
 // Load.
 async function appLoad() {
 
-	// Load query params.
+	// Load query levels.
 	let keys = picoKeys();
 	for (let k = 0; k < keys.length; k++) {
 		let value = picoStrings(k);
 		if (value) {
-			if (value.match(/x/i)) {
-				param0 = "*"; // Mul probrem.
-			} else if (value[0] == "1") {
-				param0 = "+"; // Add probrem.
-			} else {
-				param0 = "-"; // Add/Sub probrem.
-			}
 			let numbers = picoNumbers(keys[k]);
-			param1 = numbers[0];
-			param2 = numbers[1];
-			seed = numbers[2];
+			if (value.match(/x/i)) {
+				levels[0][0] = "x"; // Mul probrem.
+				levels[0][1] = numbers[0];
+				levels[0][2] = numbers[1];
+				seed = numbers[2];
+			} else {
+				levels[0][0] = "+"; // Add/Sub probrem.
+				levels[0][1] = numbers[0];
+				levels[0][2] = null;
+				seed = numbers[1];
+			}
 			picoRandom(0, seed);
-			level = -1; // Customi level.
+			level = 0; // Custom level.
 		}
 	}
 
 	// Update select button.
-	if (level < 0) {
-		picoLabel("select", param0  + param1);
-	} else {
-		picoLabel("select", "" + levels[level]);
-	}
+	picoLabel("select", appProbremText(levels[level][0], levels[level][1]));
 	picoLabel("minus", "-");
 	picoLabel("plus", "+");
 }
@@ -130,12 +117,9 @@ async function appTitle() {
 		playing = 1;
 	}
 
-	// Draw probrem sample.
-	if (level < 0) {
-		await picoChar("" + param1 + param0 + param2, -1, 0,0, 0,8);
-	} else {
-		await picoChar(samples[level], -1, 0,0, 0,8);
-	}
+	// Draw probrem title.
+	let probrem = appProbremText(levels[level][0], levels[level][1], levels[level][2]);
+	await picoChar(probrem, -1, 0,0, 0,8);
 
 	// Wait for input.
 	if (picoAction()) {
@@ -155,40 +139,33 @@ async function appProbrem() {
 		seed = picoDate();
 		picoRandom(0, seed);
 
-		// Add probrem (under 100).
-		if (param0 == "+") {
-			operator = "+";
-			if (picoRandom(2)) { // Add probrem (half size).
-				answer = picoRandom(param1/2-2) + 2; // Over 2.
-			} else { // Add probrem.
-				answer = picoRandom(param1-2) + 2; // Over 2.
-			}
-			probrem1 = picoRandom(answer - 1) + 1; // Not 0.
-			probrem2 = answer - probrem1;
-			scale = 6, scale2 = 18;
-
-			// Generate wrong answer.
-			correct = answer > 2 ? picoRandom(3) : answer > 1 ? picoRandom(2) : 0;
-			if (correct == 0) {
-				choices = [answer, answer+1, answer+2];
-			} else if (correct == 1) {
-				choices = [answer-1, answer, answer+1];
-			} else if (correct == 2) {
-				choices = [answer-2, answer-1, answer];
-			}
-
-		// Add/Sub probrem (under 100).
-		} else if (param0 == "-") {
-			operator = picoRandom(2) ? "+" : "-";
-			if (operator == "+") { // Add probrem.
-				answer = picoRandom(param1);
-				probrem1 = picoRandom(answer);
+		// Add/Sub probrem.
+		if (levels[level][0] != "x") {
+			if (levels[level][1] < 10) {
+				let n = levels[level][1] > 0 ? levels[level][1] : 1;
+				operator = "+";
+				if (picoRandom(2)) { // Add probrem (half size).
+					answer = picoRandom(n - 2) + 2; // Half size over 2.
+				} else { // Add probrem.
+					answer = picoRandom(10 - 2) + 2; // Over 2.
+				}
+				probrem1 = picoRandom(answer - 1) + 1; // Not 0.
 				probrem2 = answer - probrem1;
-			} else { // Sub probrem.
-				probrem1 = picoRandom(param1-1) + 1; // Not 0.
-				probrem2 = picoRandom(probrem1 + 1);
-				answer = probrem1 - probrem2;
+			} else {
+				if (picoRandom(2)) { // Sub probrem.
+					operator = "-";
+					probrem1 = picoRandom(levels[level][1]);
+					probrem2 = picoRandom(probrem1 + 1);
+					answer = probrem1 - probrem2;
+				} else { // Add probrem.
+					operator = "+";
+					answer = picoRandom(levels[level][1]);
+					probrem1 = picoRandom(answer);
+					probrem2 = answer - probrem1;
+				}
 			}
+
+			// Add/Sub probrem (under 99).
 			scale = 6, scale2 = 18;
 
 			// Generate wrong answer.
@@ -201,13 +178,21 @@ async function appProbrem() {
 				choices = [answer-2, answer-1, answer];
 			}
 
-		// Mul probrem (under 100).
-		} else if (param1 * param2 < 100) {
-			probrem1 = picoRandom(param1) + 1;
-			probrem2 = picoRandom(param2) + 1;
+		// Mul probrem (under 12x12).
+		} else if (levels[level][1] <= 12 && levels[level][2] <= 12) {
+			probrem1 = picoRandom(levels[level][1]) + 1;
+			probrem2 = picoRandom(levels[level][2]) + 1;
 			operator = "*";
 			answer = probrem1 * probrem2;
-			scale = 6, scale2 = 18;
+
+			// Mul probrem (under 99).
+			if (levels[level][1] * levels[level][2] <= 99) {
+				scale = 6, scale2 = 18;
+
+			// Mul probrem (over 100).
+			} else {
+				scale = 4, scale2 = 17;
+			}
 
 			// Generate wrong answer.
 			correct = answer >= probrem1*2 ? picoRandom(3) : answer >= probrem1 ? picoRandom(2) : 0;
@@ -219,20 +204,20 @@ async function appProbrem() {
 				choices = [probrem1 * (probrem2-2), probrem1 * (probrem2-1), answer];
 			}
 
-		// Mul probrem (over 100).
+		// Mul probrem (over 12).
 		} else {
-			probrem1 = picoRandom(param1-10) + 11;
-			probrem2 = picoRandom(param2-1) + 2;
+			probrem1 = picoRandom(levels[level][1]-10) + 11; // Over 11.
+			probrem2 = picoRandom(levels[level][2]-1) + 2;
 			operator = "*";
 			answer = probrem1 * probrem2;
 
-			// Mul probrem (under 1000).
-			if (param1 * param2 < 1000) {
-				scale = 4, scale2 = 16;
+			// Mul probrem (under 999).
+			if (levels[level][1] * levels[level][2] <= 999) {
+				scale = 4, scale2 = 17;
 
 			// Mul probrem (over 1000).
 			} else {
-				scale = 3, scale2 = 16;
+				scale = 3, scale2 = 17;
 			}
 
 			// Generate wrong answer.
@@ -244,14 +229,6 @@ async function appProbrem() {
 			} else if (correct == 2) {
 				choices = [answer-20, answer-10, answer];
 			}
-		}
-
-		// Spacer.
-		if (probrem1 < 10) {
-			probrem1 = "" + probrem1 + " ";
-		}
-		if (probrem2 < 10) {
-			probrem2 = " " + probrem2 + "";
 		}
 
 		// Reset playing count.
@@ -268,10 +245,11 @@ async function appProbrem() {
 	}
 
 	// Draw number.
-	await picoChar("" + number + "/" + numberMax, 0, 0,-85, 0,2);
+	await picoChar("" + number + "/" + maxnumber, 0, 0,-85, 0,2);
 
 	// Draw probrem.
-	await picoChar("" + probrem1 + operator + probrem2, -1, 0,-50, 0,scale);
+	let probrem = appProbremText(operator, probrem1, probrem2);
+	await picoChar(probrem, -1, 0,-50, 0,scale);
 
 	// Draw answer.
 	for (let i = 0; i < 3; i++) {
@@ -310,10 +288,11 @@ async function appAnswer() {
 	}
 
 	// Draw number.
-	await picoChar("" + number + "/" + numberMax, 0, 0,-85, 0,2);
+	await picoChar("" + number + "/" + maxnumber, 0, 0,-85, 0,2);
 
 	// Draw probrem.
-	await picoChar("" + probrem1 + operator + probrem2, -1, 0,-50, 0,scale);
+	let probrem = appProbremText(operator, probrem1, probrem2);
+	await picoChar(probrem, -1, 0,-50, 0,scale);
 
 	// Draw choose answer.
 	if (choose != correct) {
@@ -331,7 +310,7 @@ async function appAnswer() {
 		// Go next probrem or show result.
 		if (choose == correct) {
 			number++;
-			if (number > numberMax) {
+			if (number > maxnumber) {
 				state = "result";
 			} else {
 				state = "probrem";
@@ -367,12 +346,19 @@ async function appResult() {
 		// Enable share button.
 		picoLabel("action", "^");
 
+		// Unlock extra levels.
+		let maxlevel = levels.length-1 - maxextra + extra;
+		if (level == maxlevel) {
+			extra = extra + 1 < maxextra ? extra + 1 : maxextra;
+		}
+
 		// Reset playing count.
 		playing = 1;
 	}
 
-	// Draw probrem sample.
-	await picoChar(samples[level], -1, 0,-85, 0,2);
+	// Draw probrem title.
+	let probrem = appProbremText(levels[level][0], levels[level][1], levels[level][2]);
+	await picoChar(probrem, -1, 0,-85, 0,2);
 	await picoChar(seed, 0, 0,-75, 0,1);
 
 	// Draw result.
