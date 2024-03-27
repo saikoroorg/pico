ca706622284813a0643209e3d0d173096c854b47
@ <@>
2024-03-27 16:06:29 +0900

    Add voxel app.

---
+++ app/voxel.js
@@ -0,0 +1,243 @@
+const title = "Voxel"; // Title.
+const editjs = "app/edit.js"; // Editor script.
+const maxdepth = 40; // Maximum of depth.
+const adddepth = 1; // Additions of depth.
+const depth0 = 4; // Initial depth.
+var count = 1; // Count of dice.
+var depth = 0; // Depth.
+var maximum = 6; // Maximum of dice faces.
+var maxmaximum = 20; // Maximum of numbered dice.
+var playing = 0; // Playing count.
+
+// Voxel class.
+Voxel = class {
+	static scale = [0.01,0.01,0.01];
+	static normals = [
+		[1,0,0],[-1,0,0],
+		[0,1,0],[0,-1,0],
+		[0,0,1],[0,0,-1],
+	];
+	static vertexes = [
+		[[1,0,1],[1,0,0],[1,1,1],[1,1,0],[1,1,1],[1,0,0]],
+		[[0,0,0],[0,0,1],[0,1,0],[0,1,1],[0,1,0],[0,0,1]],
+		[[0,1,1],[1,1,1],[0,1,0],[1,1,0],[0,1,0],[1,1,1]],
+		[[1,0,1],[0,0,1],[1,0,0],[0,0,0],[1,0,0],[0,0,1]],
+		[[0,0,1],[1,0,1],[0,1,1],[1,1,1],[0,1,1],[1,0,1]],
+		[[1,0,0],[0,0,0],[1,1,0],[0,1,0],[1,1,0],[0,0,0]],
+	];
+
+	// constructor.
+	constructor() {
+		this.colors = [255,255,255, 0,0,0]; // Original design colors.
+		this.pixels = [
+		]; // Original design pixels.
+	}
+
+	// Get normal string.
+	normalString(v0) {
+		let r = "";
+		for (let i=0; i<v0.length; i++) {
+			r += v0[i] ? v0[i] +"e+01" : "0e+00";
+			r += " ";
+		}
+		return r;
+	}
+
+	// Get vertex string.
+	vertexString(v1, v0, s) {
+		let r = "", v=[(v1[0]+v0[0])*s[0], (v1[1]+v0[1])*s[1], (v1[2]+v0[2])*s[2]];
+		for (let i=0; i<v.length; i++) {
+			r += v[i] ? v[i] +"e+01" : "0e+00";
+			r += " ";
+		}
+		return r;
+	}
+
+	// Get vec.
+	stlPolygon(v) {
+		let poly = "";
+		for (let i=0; i<Voxel.normals.length; i++) {
+			poly += "\tfacet normal " + this.normalString(Voxel.normals[i]) + "\n";
+			poly += "\t\touter loop\n"
+			for (let j=0; j<Voxel.vertexes[i].length; j++) {
+				poly += "\t\t\tvertex " + this.vertexString(v, Voxel.vertexes[i][j], Voxel.scale) + "\n";
+			}
+			poly += "\t\tendloop\n"
+			poly += "\tendfacet\n"
+		}
+		return poly;
+	}
+
+	// Get stl file.
+	stlFile(c) {
+		let text = "solid voxel\n";
+		for (let i=0; i<this.pixels.length; i++) {
+			let x0 = -4.5, y0 = -4.5, z0 = -this.pixels.length / 2;
+			for (let j=0; j<this.pixels[i].length; j++) {
+				if (this.pixels[i][j*3+0] == 0) {
+					x0 = -this.pixels[i][j*3+1] / 2;
+					y0 = -this.pixels[i][j*3+2] / 2;
+				} else if (this.pixels[i][j*3+0] == c) {
+					let x = x0 + this.pixels[i][j*3+1];
+					let y = -this.pixels[i][j*3+2];
+					let z = z0 + i;
+					text += this.stlPolygon([x,y,z]);
+				}
+			}
+		}
+		text += "endsolid\n";
+		return picoTextFile(text);
+	}
+};
+var voxel = new Voxel();
+
+// Action button.
+async function appAction() {
+
+	// Share voxle stl file.
+	picoShare(null, [voxel.stlFile(1), voxel.stlFile(2)]);
+
+	// Share screen.
+	//picoShareScreen(); // Start sharing screen.
+}
+
+// Select button.
+async function appSelect(x) {
+	if (x) {
+		depth = depth + (x*adddepth) < 0 ? 0 : depth + (x*adddepth) < maxdepth ? depth + (x*adddepth) : maxdepth;
+		picoLabel("select", depth>0?""+depth:"*");
+		picoFlush();
+	} else 	if (depth <= 0) {
+
+		// Enter to edit mode.
+		picoResetParams();
+
+		// Enter to edit mode with custom design.
+		if (voxel.pixels.length > 0) {
+			let key = 0;
+			for (; key < voxel.pixels.length; key++) {
+				if (voxel.pixels[key].length > 0) {
+					picoSetCode6(voxel.pixels[key], key);
+				}
+			}
+			picoSetCode8(voxel.colors, key);
+		}
+
+		// Enter to edit mode.
+		picoSwitchApp(editjs); // Open editor.
+	}
+}
+
+// Load.
+async function appLoad() {
+	picoTitle(title);
+
+	// Load query params.
+	let keys = picoKeys();
+	for (let k = 0; k < keys.length; k++) {
+		let value = picoString(k);
+		if (value) {
+			console.log("Param" + k + ": " + keys[k] + " -> " + picoString(k));
+
+			// Load colors.
+			if ((value[0] == "0" && value[1] == "0" && value[2] == "0") ||
+				(value[0] == "1" && value[1] == "1" && value[2] == "1")) {
+				voxel.colors = picoCode8(keys[k]);
+
+			// Load pixels.
+			} else if (value[0] == "0" && value[1] != "0" && value[2] != "0" && value.length >= 6) {
+				voxel.pixels[voxel.pixels.length] = picoCode6(keys[k]);
+				maxmaximum = maximum = voxel.pixels.length;
+				custom = true;
+			}
+		}
+	}
+	picoColor(voxel.colors);
+
+	picoLabel("action", "&");
+	picoLabel("select", depth>0?""+depth:"*");
+	picoLabel("minus", "-");
+	picoLabel("plus", "+");
+}
+
+var angle = 0; // Rolling angle.
+var scale = 9; // Rolling scale.
+
+// Main.
+async function appMain() {
+
+	// Initialize.
+	if (playing <= 0) {
+		result = 0;
+
+		// Reset playing count.
+		playing = 1;
+	}
+
+	// Update rolling dice.
+	if (depth <= 0) {
+		if (result > 0) {
+
+			// Restart.
+			if (picoMotion()) {
+				playing = -1;
+			}
+
+		} else {
+
+			// Holding.
+			if (picoMotion()) {
+				playing = 1;
+			}
+		}
+	}
+
+	// Draw customizing dice.
+	if (depth <= 0) {
+
+		// Draw icon.
+		let s1 = 10;
+		if (picoAction()) {
+			depth = depth0;
+			playing = -1;
+			picoLabel("select", depth>0?""+depth:"*");
+			picoBeep(1.2, 0.1);
+		} else if (picoMotion()) {
+			s1 = 8;
+		}
+
+		// Draw original design sprite.
+		for (let i = voxel.pixels.length-1; i >= 0; i--) {
+			picoSprite(voxel.pixels[i], -1, 0,0, angle, s1);
+		}
+
+	// Draw rolling dice.
+	} else {
+
+		// Draw icon.
+		let s1 = 10;
+		if (picoAction()) {
+			depth = 0;
+			playing = -1;
+			picoLabel("select", depth>0?""+depth:"*");
+			picoBeep(1.2, 0.1);
+		} else if (picoMotion()) {
+			s1 = 8;
+		}
+
+		// Draw original design sprite.
+		let x0 = -voxel.pixels.length*depth/2, y0 = voxel.pixels.length*depth/2;
+		for (let i = voxel.pixels.length-1; i >= 0; i--) {
+			let x = x0+i*depth, y = y0-i*depth;
+			picoSprite(voxel.pixels[i], -1, x,y, angle, s1);
+		}
+	}
+
+	// Update animation if rolling.
+	if (result == 0 || playing < 5) {
+		picoFlush();
+	}
+
+	// Increment playing count.
+	playing++;
+};
