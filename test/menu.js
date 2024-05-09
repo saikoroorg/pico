93937f1852e2f8ce7b16fcfb205205907b8ea9a5
@ <@>
2024-05-09 10:41:08 +0900

    Add noheader option for title.

--- test/menu.js
+++ test/menu.js
@@ -1,15 +1,15 @@
 const title = "1234567.890"; // Title.
 const dots = [ // Dotted design pixels.
-	[0,7,7],
-	[0,7,7, 5,3,3],
-	[0,7,7, 4,1,5, 4,5,1],
-	[0,7,7, 6,3,3, 6,1,5, 6,5,1],
-	[0,7,7, 8,1,1, 8,1,5, 8,5,1, 8,5,5],
-	[0,7,7, 7,3,3, 7,1,1, 7,1,5, 7,5,1, 7,5,5],
-	[0,7,7, 9,1,1, 9,1,3, 9,1,5, 9,5,1, 9,5,3, 9,5,5],
-	[0,7,7, 2,3,3, 2,1,1, 2,1,3, 2,1,5, 2,5,1, 2,5,3, 2,5,5],
-	[0,7,7, 1,1,1, 1,1,3, 1,3,1, 1,3,5, 1,1,5, 1,5,1, 1,5,3, 1,5,5],
 	[0,7,7, 3,3,3, 3,1,1, 3,1,3, 3,3,1, 3,3,5, 3,1,5, 3,5,1, 3,5,3, 3,5,5],
+	[0,7,7, 0,0,0,0,6,6, 9,3,3],
+	[0,7,7, 0,0,0,0,6,6, 9,1,5, 9,5,1],
+	[0,7,7, 0,0,0,0,6,6, 9,3,3, 9,1,5, 9,5,1],
+	[0,7,7, 0,0,0,0,6,6, 9,1,1, 9,1,5, 9,5,1, 9,5,5],
+	[0,7,7, 0,0,0,0,6,6, 9,3,3, 9,1,1, 9,1,5, 9,5,1, 9,5,5],
+	[0,7,7, 0,0,0,0,6,6, 9,1,1, 9,1,3, 9,1,5, 9,5,1, 9,5,3, 9,5,5],
+	[0,7,7, 0,0,0,0,6,6, 9,3,3, 9,1,1, 9,1,3, 9,1,5, 9,5,1, 9,5,3, 9,5,5],
+	[0,7,7, 0,0,0,0,6,6, 9,1,1, 9,1,3, 9,3,1, 9,3,5, 9,1,5, 9,5,1, 9,5,3, 9,5,5],
+	[0,7,7, 0,0,0,0,6,6, 9,3,3, 9,1,1, 9,1,3, 9,3,1, 9,3,5, 9,1,5, 9,5,1, 9,5,3, 9,5,5],
 ];
 const devitems = [ // Menu items for dev.
 	["dice", "app/dice.svg", "app/dice.js"],
@@ -24,6 +24,7 @@ const devitems = [ // Menu items for dev.
 	["edit", "app/edit.svg", "app/edit.js"],
 	["text", "test/text.svg", "test/text.js"],
 	["hex", "test/hex.svg", "test/hex.js"],
+	["demo", "test/demo.svg", "test/demo.js"],
 //*/
 ];
 const devreturl = null; // Return url for dev.
@@ -35,31 +36,38 @@ var items = [ // Menu items.
 	["chess", "chess/icon.svg", "chess/"],
 	["shogi", "shogi/icon.svg", "shogi/"],
 ];
-var returl = "../"; // Return url.
+var returl = ".."; // Return url.
 var images = []; // Menu images.
+var state = "demo"; // Playing state.
 var playing = 0; // Playing count.
-const mincount = 3; // Minumum item count.
-var count = 0; // Item count.
+var index = 0; // Sprite index.
 var angle = 0; // Rolling angle.
 var scale = 1; // Rolling scale.
 
-// Select button.
-async function appSelect() {
-	if (count == mincount) {
-		picoTitle(title); // Title.
-		picoSpriteData(dots[random], 0).then((image) => {
-			picoLabel("select", null, image); // Lazy loading.
-		});//*/
-		count = items.length;
-	} else {
-		picoTitle(title, true); // No title label.
-		picoSpriteData(dots[9], -1).then((image) => {
-			picoLabel("select", null, image); // Lazy loading.
+// Update buttons.
+async function appUpdate() {
+	if (state != "demo") {
+		picoTitle(title, true); // No label.
+		picoSpriteData(dots[index], -1).then((image) => {
+			picoLabel("action", null, image); // Lazy loading.
 		});
-		playing = -1; // Reroll.
-		count = mincount;
+	} else {
+		picoTitle(title, true, true); // No label and header.
+		picoLabel("action", null);
 	}
 	picoFlush();
+}
+
+// Action button.
+async function appAction() {
+	if (state != "demo") {
+		state = "demo";
+		playing = -1; // Reroll.
+	} else {
+		state = "menu";
+		playing = 0;
+	}
+	appUpdate(); // Update buttons.
 }
 
 // Load.
@@ -68,6 +76,8 @@ async function appLoad() {
 		items = devitems;
 		returl = devreturl;
 	}
+
+	// Load images.
 	for (let i = 0; i < items.length; i++) {
 		if (items[i][1]) {
 			picoLoad(items[i][1]).then((image) => {
@@ -77,8 +87,14 @@ async function appLoad() {
 		}
 	}
 
+	// Skip demo on continuous start.
+	let keys = picoKeys();
+	if (keys.length > 0) {
+		state = "menu";
+	}
+
 	appResize(); // Initialize positions.
-	appSelect(); // Initialize select button.
+	appUpdate(); // Initialize buttons.
 }
 
 var landscape = false; // landscape mode.
@@ -93,17 +109,18 @@ async function appResize() {
 async function appMain() {
 
 	// Bg.
-	const bgcolor = 1;
-	picoRect(bgcolor, 0,0, landscape?200:116,landscape?140:200, 0,1);
+	const bgcolor = 1, bgscale = 1;
+	picoRect(bgcolor, 0,0, landscape?200:140,landscape?140:200, 0,bgscale);
 
 	// Dice.
-	if (count <= mincount) {
-		const maximum = 6;
-		const dicescale = 8, dotswidth = 32;
-		const diceoffset = -36;
-		if (picoAction(0,diceoffset, dotswidth,dotswidth)) {
-			playing = -1; // Reroll.
-		} else if (picoMotion(0,diceoffset, dotswidth,dotswidth)) {
+	if (state == "demo") {
+		const maximum = 6, dicescale = 8, diceoffset = -12;
+		if (playing > 60 && picoAction()) {
+			state = "menu";
+			//playing = -1; // Reroll.
+			appUpdate(); // Show menu.
+			return;
+		} else if (picoMotion()) {
 			if (playing <= 60) {
 				playing = 0;
 			}
@@ -113,47 +130,50 @@ async function appMain() {
 		}
 		if (playing <= 60) {
 			angle = picoMod(angle + 20, 360);
-			random = picoRandom(maximum) + 1;
+			index = picoRandom(maximum) + 1;
 			picoFlush(); // Update animation without input.
 		} else {
 			angle = 0;
 		}
-		picoSprite(dots[random], 0, 0,diceoffset, angle,dicescale*scale);
+		picoSprite(dots[index], 0, 0,diceoffset, angle,dicescale*scale);
 	}
 
 	// Logo.
-	if (count <= mincount) {
-		const logocolor = 5, logooffset = 14, logoscale = 2.5;
+	if (state == "demo") {
+		const logocolor = 5, logoscale = 2.5, logooffset = 38;
 		picoChar(title, logocolor, 0,logooffset, 0,logoscale);
 	}
 
 	// Menu.
-	const itemcolor = 2, itemscale = 1.5;
-	const itemwidth = 32, itemvgrid = 40, itemhgrid = 52, imagescale = 0.4;
-	const itemoffset = count <= mincount ? 52 : 0;
-	let xcount = count < 6 ? 3 : !landscape ? 3 : picoSqrt(count - 1) + 1;
-	let ycount = picoDiv(count - 1, xcount) + 1;
-	for (let i = 0; i < count; i++) {
-		let x = (picoMod(i, xcount) - (xcount - 1) / 2) * itemvgrid;
-		let y = (picoDiv(i, xcount) - (ycount - 1) / 2) * itemhgrid + itemoffset;
-		let s = picoMotion(x,y, itemwidth/2,itemwidth/2) ? 0.9 : 1;
-		if (picoAction(x,y, itemwidth/2,itemwidth/2)) {
-			if (items[i][2]) {
-				picoSwitchApp(items[i][2], returl);
+	if (state == "menu") {
+		// 300(Image size) * 0.4(Image scale) / 4(Pixel ratio) = 30(Pixel size)
+		const itemcolor = 2, itemscale = 1.5, imagescale = 0.4;
+		const itemwidth = 30, itemvgrid = 44, itemhgrid = 44;
+		const itemoffset = -4, textoffset = 16;
+		let xcount = items.length < 6 ? 3 : !landscape ? 3 : picoSqrt(items.length - 1) + 1;
+		let ycount = picoDiv(items.length - 1, xcount) + 1;
+		for (let i = 0; i < items.length; i++) {
+			let x = (picoMod(i, xcount) - (xcount - 1) / 2) * itemvgrid;
+			let y = (picoDiv(i, xcount) - (ycount - 1) / 2) * itemhgrid;
+			let s = picoMotion(x,y, itemwidth/2,itemwidth/2) ? 0.9 : 1;
+			if (picoAction(x,y, itemwidth/2,itemwidth/2)) {
+				if (items[i][2]) {
+					picoSwitchApp(items[i][2], returl);
+				}
 			}
-		}
-		picoRect(itemcolor, x,y, itemwidth,itemwidth, 0,s);
-		if (images[i]) {
-			picoImage(images[i], x,y, 0,imagescale*s)
-			if (count > mincount) {
-				picoChar(items[i][0], 2, x,y+itemwidth/2+8, 0,itemscale);
+			picoRect(itemcolor, x,y+itemoffset, itemwidth,itemwidth, 0,s);
+			if (images[i]) {
+				picoImage(images[i], x,y+itemoffset, 0,imagescale*s)
+				picoChar(items[i][0], 2, x,y+textoffset, 0,itemscale);
+			} else {
+				if (items[i][0].length <= 5) {
+					picoChar(items[i][0], 0, x,y+textoffset, 0,itemscale*s);
+				} else if (items[i][0].length <= 8) {
+					picoText(items[i][0], 0, x,y+textoffset,4*4,6*2, 0,itemscale*s);
+				} else {
+					picoText(items[i][0], 0, x,y+textoffset,4*4,6*3, 0,itemscale*s);
+				}
 			}
-		} else if (items[i][0].length <= 5) {
-			picoChar(items[i][0], 0, x,y, 0,itemscale*s);
-		} else if (items[i][0].length <= 8) {
-			picoText(items[i][0], 0, x,y,4*4,6*2, 0,itemscale*s);
-		} else {
-			picoText(items[i][0], 0, x,y,4*4,6*3, 0,itemscale*s);
 		}
 	}
 
