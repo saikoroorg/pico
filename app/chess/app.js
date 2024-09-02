b2b70cf3c7474898c798ac952ca8a5ec7a820a47
@ <@>
2024-09-02 21:13:23 +0900

    Fix chess log code on board pieces.

--- app/chess/app.js
+++ app/chess/app.js
@@ -78,6 +78,11 @@ Playlog = class {
 
 	// constructor.
 	constructor() {
+		this.clear();
+	}
+
+	// Clear log.
+	clear() {
 		this.moves = [];
 		this.undos = [];
 	}
@@ -415,7 +420,13 @@ async function appSelect(x) {
 			picoFlush();
 		}
 
-	// Reverse board.
+	// Clear logs.
+	} else {
+		playlog.clear();
+		picoLabel("select", ""+playlog.count());
+	}
+
+	/*// Reverse board.
 	} else {
 		reverse = reverse ? 0 : 1;
 		if (yflip) {
@@ -424,7 +435,7 @@ async function appSelect(x) {
 			}
 		}
 		picoFlush();
-	}
+	}*/
 }
 
 // Action button.
